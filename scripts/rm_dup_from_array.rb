366717c91bb0be2a8273f11d68c5bc44c153410f
mahbubzaman <mahbubzaman17@gmail.com>
2020-04-17 23:19:47 +1000

    Add files via upload

---
+++ scripts/rm_dup_from_array.rb
@@ -0,0 +1,24 @@
+a = [
+"Drama",
+"Mystery",
+"Drama",
+"Mystery",
+"Drama",
+"Mystery",
+"Drama",
+"Crime",
+"Sci-fi",
+"Disaster",
+"Thriller",
+"Action",
+"Drama",
+"Crime",
+"Drama",
+"Mystery",
+"Drama",
+"Mystery",
+"Drama"
+]
+
+a = a.uniq
+puts a
