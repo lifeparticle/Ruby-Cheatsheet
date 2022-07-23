5eeccf8c431aabaa9792dd64532ad9a4eff06fc8
Mahbub Zaman <mahbubzaman17@gmail.com>
2022-07-23 11:53:50 +1000

    Update build_readme.py

--- build_readme.py
+++ build_readme.py
@@ -3,9 +3,9 @@ import pathlib
 import json
 import re
 
-# https://www.ruby-lang.org/en/feeds/news.rss
+RUBY_DOWNLOADS_URL =  "https://www.ruby-lang.org/en/downloads/"
+RUBY_RSS_URL = "https://api.rss2json.com/v1/api.json?rss_url=https://www.ruby-lang.org/en/feeds/news.rss"
 
-RUBY_LATEST_SOURCE_URL =  "https://www.ruby-lang.org/en/downloads/"
 root = pathlib.Path(__file__).parent.resolve()
 
 def replace_chunk(content, marker, chunk, inline=False):
@@ -34,41 +34,34 @@ def fetch_blog_posts(link):
 	return result
 
 def fetch_ruby_latest_version():
-	response = requests.get(RUBY_LATEST_SOURCE_URL)
-
+	response = requests.get(RUBY_DOWNLOADS_URL)
 	if (response.status_code != 200):
-		raise RuntimeError(f"Error while getting {RUBY_LATEST_SOURCE_URL}")
-
+		raise RuntimeError(f"Error while getting {RUBY_DOWNLOADS_URL}")
+ 	# search using the following regular expression and return the first occurrence
 	return re.search("http.*ruby-(.*).tar.gz", response.text).groups()[0]
 
 if __name__ == "__main__":
 	readme = root / "README.md"
-
 	readme_contents = readme.open().read()
 	rewritten = readme_contents
+	posts = fetch_blog_posts(RUBY_RSS_URL)
 
-	posts = fetch_blog_posts("https://api.rss2json.com/v1/api.json?rss_url=https://www.ruby-lang.org/en/feeds/news.rss")
 	if len(posts) != 0:
 		# markdown formatting
 		posts_md = "\n".join(
 			["* [{title}]({link}) <br/> <sub>{pubDate}</sub>".format(**post) for post in posts]
 		)
-		print(posts_md)
 		rewritten = replace_chunk(rewritten, "news", posts_md)
 
-	readme.open("w").write(rewritten)
-
 	# Update the doc links to point the latest ruby version docs
-	stored_ruby_latest_version_file = root / ".ruby-latest-version"
-	stored_ruby_latest_version = stored_ruby_latest_version_file.open().read().strip()
+	current_stable_version = root / "current_stable_version.txt"
+	current_stable_version_contents = current_stable_version.open().read().strip()
 	ruby_latest_version = fetch_ruby_latest_version()
 
-	if (stored_ruby_latest_version != ruby_latest_version):
-		stored_ruby_latest_version_file.open("w").write(ruby_latest_version)
-		readme_contents = readme.open().read()
-
-		doc_links = re.search("<!-- doc_links starts -->(.*)<!-- doc_links ends -->", readme_contents, re.DOTALL).groups()[0].strip()
+	if (current_stable_version_contents != ruby_latest_version):
+		doc_links = re.search("<!-- doc_links starts -->(.*)<!-- doc_links ends -->", rewritten, re.DOTALL).groups()[0].strip()
 		updated_doc_links = re.sub("core-\d.\d.\d", f"core-{ruby_latest_version}", doc_links)
+		rewritten = replace_chunk(readme_contents, "doc_links", updated_doc_links)
+		current_stable_version.open("w").write(ruby_latest_version)
 
-		readme_with_updated_doc_links = replace_chunk(readme_contents, "doc_links", updated_doc_links)
-		readme.open("w").write(readme_with_updated_doc_links)
+	readme.open("w").write(rewritten)
